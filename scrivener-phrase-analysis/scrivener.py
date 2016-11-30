3d349efd0b5c2041b36d4cc3de7ffb8469eeb4e1
Stephen Granade <stephen.granade@dynetics.com>
2016-11-30 13:03:06 -0600

    Minor refactored name changes in Scrivener processing script.

--- scrivener-phrase-analysis/scrivener.py
+++ scrivener-phrase-analysis/scrivener.py
@@ -13,9 +13,10 @@ def _fast_iter(context, func):
     del context
 
 
-def _process_binder_item(elem: etree.Element, scrivenings: dict):
+def _select_compiled_binder_item(elem: etree.Element, scrivenings: dict):
     item_id = elem.get('ID')
     item_type = elem.get('Type')
+    # Only work with text or folder items
     if item_type != 'Text' and item_type != 'Folder':
         return
 
@@ -30,19 +31,20 @@ def _process_binder_item(elem: etree.Element, scrivenings: dict):
                 if inclusion.text == 'Yes':
                     include_in_compile = True
         else:
+            # We've got to recursively process any kids, since this may be a collection
             for binder_item in sub_elem.iterchildren('BinderItem'):
-                _process_binder_item(binder_item, scrivenings)
+                _select_compiled_binder_item(binder_item, scrivenings)
 
     if include_in_compile:
         scrivenings[item_id] = title
 
 
-def get_scrivener_index_filename(basedir):
+def get_scrivener_project_filename(basedir: str):
     """
     Find the base Scrivener file in a Scrivener project.
 
     :param basedir: The Scrivener project directory.
-    :return: The Scrivener index file's name.
+    :return: The Scrivener project file's name.
     """
     files = glob.glob(os.path.join(os.path.dirname(basedir), '**/*.scrivx'), recursive=True)
 
@@ -53,14 +55,20 @@ def get_scrivener_index_filename(basedir):
             basedir, ", ".join(files)
         ))
 
-    return files
+    return files[0]
 
 
-def find_compiled_scrivenings(stream):
+def get_compiled_scrivenings(stream):
+    """
+    Find all pieces of text in the binder that are compiled to the final output.
+
+    :param stream: Open bytes stream containing the Scrivener project file.
+    :return: Ordered dictionary whose keys are the scrivening IDs and whose values are the title.
+    """
     scrivenings = OrderedDict()
     context = etree.iterparse(stream, events=('end',), tag='BinderItem')
 
     _fast_iter(context, lambda elem:
-        _process_binder_item(elem, scrivenings))
+        _select_compiled_binder_item(elem, scrivenings))
 
-    print(scrivenings)
+    return scrivenings
